d9ce7700907e63a8d962ace19cbaef0b34b54422
Siddharth Chugh <63421456+Syd25@users.noreply.github.com>
2020-07-29 05:17:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
+const serverless=require('serverles-http')
+
+app.use(express.static(__dirname + "/public"))
+let clients = 0
+
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
+    })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
+})
+
+function Disconnect() {
+    if (clients > 0) {
+        if (clients <= 2)
+            this.broadcast.emit("Disconnect")
+        clients--
+    }
+}
+
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer)
+}
+
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data)
+}
+
+module.exports.handler=serverless(app);
+
+//http.listen(port, () => console.log(`Active on ${port} port`))
+
+
+
